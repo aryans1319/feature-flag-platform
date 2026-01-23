80466ca986ecfd26d991265ec11638174fa1ceb4
aryans1319 <aryan4p130202@gmail.com>
2026-01-24 03:36:59 +0530

    fixed issues

--- src/main/java/com/aryan/featureflags/controller/FeatureEvaluationController.java
+++ src/main/java/com/aryan/featureflags/controller/FeatureEvaluationController.java
@@ -1,9 +1,13 @@
 package com.aryan.featureflags.controller;
 
 import com.aryan.featureflags.dto.EvaluationContextDto;
+import com.aryan.featureflags.dto.FeatureResponseDto;
+import com.aryan.featureflags.dto.UpdateRolloutRequestDto;
 import com.aryan.featureflags.engine.RolloutEvaluator;
 import com.aryan.featureflags.model.Environment;
 import com.aryan.featureflags.service.FeatureEvaluationService;
+import jakarta.validation.Valid;
+import org.hibernate.generator.values.internal.GeneratedValuesImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +38,18 @@ public class FeatureEvaluationController {
         return ResponseEntity.ok(
                 Map.of("enabled", result)
         );
+    }
+    @PutMapping("/{key}/rollout")
+    public ResponseEntity<FeatureResponseDto> updateRollout(
+            @PathVariable String key,
+            @RequestParam Environment env,
+            @Valid @RequestBody UpdateRolloutRequestDto request
+    ){
+        FeatureResponseDto response= evaluationService.updateRolloutPercentage(key, env, request);
+
+        return ResponseEntity.ok(response);
+
+
+
     }
 }
